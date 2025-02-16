bc64cb31adbdbf79c5a911fb85e84c4eba8e091d
Luis <Luisre01@ucm.es>
2025-02-16 20:42:06 +0100

    Leectura de tres archivos(inCifras,inLetras y diccionario) casos resueltos satisfactoriamente

--- Luis/Source.cpp
+++ Luis/Source.cpp
@@ -14,14 +14,14 @@
 #include "DFS.h"
 #include "Factory.h"
 
-/*Segunda parte letras*/
+/*Segunda parte letras*/ //compilar ctl+shift+b
 #include "Trie.h"
 
 using namespace std;
 
 
 
-void resuelveCaso(string mode) {
+void resuelveCaso(string mode, istream& archivo ) {
 
 	//numero al que queremos llegar o aproximarnos lo maximo posible
 	int numObjetivo;
@@ -29,10 +29,10 @@ void resuelveCaso(string mode) {
 	//cifras con las que hemos de operar
 	vector<int> numerosCandidatos(CIFRAS_INICIALES);
 
-	cin >> numObjetivo;
+	archivo >> numObjetivo;
 
 	for (int i = 0; i < CIFRAS_INICIALES; ++i)
-		cin >> numerosCandidatos[i];
+		archivo >> numerosCandidatos[i];
 
 	auto search = Factory::create(mode, numObjetivo, numerosCandidatos);
 
@@ -40,32 +40,23 @@ void resuelveCaso(string mode) {
 
 }
 /*Segunda Parte Letras*/
-void resuelveCasoLetras() {
-	int numPalabras;
-	cin >> numPalabras;
-
-	Trie arbolPrefijos;
-	string palabra;//IMPORTANTE HACER IGNORE CASE AQUI O EN LA CLASE TRIE?
-	for (int i = 0; i < numPalabras; ++i) {//podriamos ignorar las palabras de mas de 10 letras 
-		cin >> palabra;
-		arbolPrefijos.insert(palabra);
-	}
-
-	//caso para resolver;
+void loadDiccionario(Trie& trie, istream& archivo) {
+	string palabra;
 	
+	while (archivo>>palabra)
+		trie.insert(palabra);
+}
+void resuelveCasoLetras(Trie& trie, istream& archivo) {
 	string letrasCasoi;
 	int numProblemas;
-	cin >> numProblemas;
+	archivo >> numProblemas;
 
 	for (int i = 0; i < numProblemas; ++i) {
-		cin >> letrasCasoi;
+		archivo >> letrasCasoi;
 
-		arbolPrefijos.solve(letrasCasoi);
+		trie.solve(letrasCasoi);
 	}
-		
-
 }
-
 
 
 //@ </answer>
@@ -75,10 +66,11 @@ int main(int argc, char* argv[]) {
 	// ajustes para que cin extraiga directamente de un fichero
 
 	// Verificp si se paso un archivo como argumento
-	if (argc < 2) {
-		cerr << "Uso: " << argv[0] << " <archivo_de_entrada.txt>" << endl;
+	if (argc < 4) {
+		std::cerr << "Uso: " << argv[0] << " <archivo_casos.txt> <archivo_diccionario.txt> <archivo_casos_letras.txt>" << std::endl;
 		return 1;
 	}
+
 
 	string nombreArchivo = argv[1];
 
@@ -90,18 +82,14 @@ int main(int argc, char* argv[]) {
 	}
 
 	// Redirigir la entrada estándar desde el archivo
-	auto cinbuf = cin.rdbuf(archivoEntrada.rdbuf());
-	/*
-#ifndef DOMJUDGE
-	std::ifstream in("Texto.txt");
-	auto cinbuf = std::cin.rdbuf(in.rdbuf());
-#endif*/
+	//auto cinbuf = cin.rdbuf(archivoEntrada.rdbuf());
+	
 
 	int numCasos;
 	string mode;
 
-	std::cin >> numCasos;
-	cin >> mode;
+	archivoEntrada >> numCasos;
+	archivoEntrada >> mode;
 
 	//tiempo antes de ejecutar el algoritmo
 	auto start = std::chrono::high_resolution_clock::now();
@@ -109,10 +97,36 @@ int main(int argc, char* argv[]) {
 	cout << "Solucion para el archivo: " << nombreArchivo << " en modo " << mode << '\n';
 
 	for (int i = 0; i < numCasos; ++i)
-		resuelveCaso(mode);
+		resuelveCaso(mode, archivoEntrada);
 
 	/*Segunda parte letras*/
-	resuelveCasoLetras();
+	Trie trie;
+
+	//cargamos los datos del diccionario
+	string nombreArchivoDiccionario = argv[2];
+	// Abrimos el archivo
+	ifstream archivoEntradaDiccionario(nombreArchivoDiccionario);
+	if (!archivoEntradaDiccionario) {
+		cerr << "Error: No se pudo abrir el archivo " << nombreArchivoDiccionario << endl;
+		return 1;
+	}
+	// Redirigir la entrada estándar desde el archivo
+	//auto cinbuf = cin.rdbuf(archivoEntradaDiccionario.rdbuf());
+
+	loadDiccionario(trie, archivoEntradaDiccionario);
+
+	//cargamos los datos del diccionario
+	string nombreArchivoCasosLetras = argv[3];
+	// Abrimos el archivo
+	ifstream archivoCasosLetras(nombreArchivoCasosLetras);
+	if (!archivoCasosLetras) {
+		cerr << "Error: No se pudo abrir el archivo " << nombreArchivoCasosLetras << endl;
+		return 1;
+	}
+	// Redirigir la entrada estándar desde el archivo
+	auto cinbuf = cin.rdbuf(archivoCasosLetras.rdbuf());
+
+	resuelveCasoLetras(trie, archivoCasosLetras);
 
 	//guardamos el tiempo despues de ejecutar el algoritmo
 	auto end = std::chrono::high_resolution_clock::now();
@@ -120,11 +134,6 @@ int main(int argc, char* argv[]) {
 
 	cout << "Tiempo en la resolucion de los " << numCasos << " casos de prueba  " << duration.count() << " milisegundos.\n";
 
-	// para dejar todo como estaba al principio
-	 /*
-#ifndef DOMJUDGE
-	std::cin.rdbuf(cinbuf);
-	system("PAUSE");
-#endif*/
+	
 	return 0;
 }
